67eed4e3b76546c5c16c9253ab47e6d2ae2e3a58
LoreB43 <lolloberti43@gmail.com>
2025-03-14 14:53:41 +0100

    modifica nuovi parametri nella validazione in frequenza

--- Poli per caso/code sys/validation_freq.m
+++ Poli per caso/code sys/validation_freq.m
@@ -8,21 +8,7 @@ add_data('\data\Sine Test');
 
 %% Model Parameters
 
-k_aero = 0;
-k_mag = 6.5308e-5; % Nm^2/A^2   assumiamo costante ma potremmo validarlo considerando  l'area calcolabile tra dai dati del datasheet
-m = 0.0657; % kg
-y0 = 0.0086; % m
-Rs = 1; % Ohm
-Rc = 8.14;%Ohm
-%Rc2 = 9.14
-%Rc3 = 8.35
-Rtot = Rs + Rc; %Ohm
-Lc = 0.3537; % H
-%Lc PDB = 0.2934
-%Lc2 = 0.2328
-%Lc3 = 0.3626
-
-theta = [k_aero, k_mag, m, y0, Rtot, Lc]';
+run('Model_Parameter.m')
 %% Electric Model
 sys_el = tf(1, [Lc, Rtot]);
 polo = Rtot/Lc;
