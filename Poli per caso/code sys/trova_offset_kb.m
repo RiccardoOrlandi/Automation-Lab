7edecc581df588051aff933daa789ea7b605270a
Riccardo Orlandi <riccardo.orlandi0803@icloud.com>
2025-03-18 16:26:55 +0100

    modificato file stima di kb

--- Poli per caso/code sys/trova_offset_kb.m
+++ Poli per caso/code sys/trova_offset_kb.m
@@ -2,9 +2,8 @@ clear all
 close all
 clc
 add_data_volt('/data/Step Test/Step Test without ball');
-add_data_volt('/data/Step Test/Step Test with ball');
-offset_posizione_volt = mean(Test_0V(2,:));
+offset_posizione_volt = mean(Test_wb_0V(2,:));
 offset_corrente_volt = mean(Test_wb_0V(3,:));
 
-
-Kb =0.0086/ mean(Test_21V(2,(end-50):end)-offset_corrente_volt);
+add_data_volt('/data/Step Test/Step Test with ball');
+Kb = 0.0086 / (mean([Test_21V(2,(end-50):end), Test_21_3V(2,(end-50):end), Test_21_6V(2,(end-50):end), Test_21_9V(2,(end-50):end), Test_20V(2,(end-50):end), Test_20_5V(2,(end-50):end)], 'all') - offset_corrente_volt);
