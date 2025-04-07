5424888486380048050e15a28857eeab6ccbb9e1
Mamt13 <andrea13savino@gmail.com>
2025-04-07 11:45:33 +0200

    Nuova disposizione file

---
+++ Poli per caso/MagneticLevitation_Template_quarc_win64/MagneticLevitation_Template_types.h
@@ -0,0 +1,32 @@
+/*
+ * MagneticLevitation_Template_types.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "MagneticLevitation_Template".
+ *
+ * Model version              : 1.63
+ * Simulink Coder version : 9.2 (R2019b) 18-Jul-2019
+ * C source code generated on : Fri Mar  7 12:58:47 2025
+ *
+ * Target selection: quarc_win64.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_MagneticLevitation_Template_types_h_
+#define RTW_HEADER_MagneticLevitation_Template_types_h_
+#include "rtwtypes.h"
+#include "multiword_types.h"
+
+/* Parameters (default storage) */
+typedef struct P_MagneticLevitation_Template_T_ P_MagneticLevitation_Template_T;
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_MagneticLevitation_Te_T RT_MODEL_MagneticLevitation_T_T;
+
+#endif                     /* RTW_HEADER_MagneticLevitation_Template_types_h_ */
