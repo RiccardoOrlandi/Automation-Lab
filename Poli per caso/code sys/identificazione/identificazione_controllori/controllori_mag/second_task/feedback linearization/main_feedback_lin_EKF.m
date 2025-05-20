84f6b65e8c876354e4266dc85c932e23fd208f3f
LoreB43 <lolloberti43@gmail.com>
2025-05-20 14:11:13 +0200

    feedback linearization con RLS

---
+++ Poli per caso/code sys/identificazione/identificazione_controllori/controllori_mag/second_task/feedback linearization/main_feedback_lin_EKF.m
@@ -0,0 +1,12 @@
+clear 
+clc 
+close all
+
+%%
+%per Windows 
+addpath('..\..\..\..\..\function')
+run('..\..\..\..\..\Model_Parameter.m')
+%per Mac
+%addpath('../../../../../function')
+%run('../../../../../Model_Parameter.m')
+
